ec19d1f0ed0dce4a440e1d9e42b7319922ffcda8
sayanmondal-sm <mondalsantosh205@gmail.com>
2026-09-03 23:53:16 +0530

    Time: 40 ms (61.69%), Space: 54.7 MB (61.01%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+     bool isPalin(string s){
+        string s2 = s;
+        reverse(s2.begin(),s2.end());
+        if(s2 == s){
+            return true;
+        }
+        return false;
+     }
+
+    void getAllPart(string s,vector<string>&partition,vector<vector<string>>&ans){
+        if(s.size() == 0){
+            ans.push_back(partition);
+            return;
+        }
+        for(int i=0; i<s.size(); i++){
+            string part = s.substr(0,i+1);
+            if(isPalin(part)){
+                partition.push_back(part);
+                getAllPart(s.substr(i+1),partition,ans);
+                partition.pop_back();
+            }
+        }
+    }
+
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>partition;
+        getAllPart(s,partition,ans);
+        return ans;
+
+        
+    }
+};
