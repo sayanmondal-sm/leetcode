ee6a47ddda829d5808062dc1f9b59d76ad8e6159
sayanmondal-sm <mondalsantosh205@gmail.com>
2026-09-08 23:15:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+       int ans=0;
+       if(n < 1000){
+        return 0;
+       }
+       if(n >=1000){
+        ans=n-999;
+       }
+       return ans; 
+    }
+};
