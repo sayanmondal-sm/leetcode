7c5f4e23cb8c1042a31eef654f9726e70541b77e
sayanmondal-sm <mondalsantosh205@gmail.com>
2026-09-19 21:50:20 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.85%) - LeetHub

---
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+            long long distance(int ux, int uy, int vx, int vy) {
+        return (long long)pow(ux - vx, 2) + (long long)pow(uy - vy, 2);
+    }
+
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1,
+                      int x2, int y2) {
+        if (x1 <= xCenter && xCenter <= x2 && y1 <= yCenter && yCenter <= y2) {
+            return true;
+        }
+        if (x1 <= xCenter && xCenter <= x2 && y2 <= yCenter &&
+            yCenter <= y2 + radius) {
+            return true;
+        }
+        if (x1 <= xCenter && xCenter <= x2 && y1 - radius <= yCenter &&
+            yCenter <= y1) {
+            return true;
+        }
+        if (x1 - radius <= xCenter && xCenter <= x1 && y1 <= yCenter &&
+            yCenter <= y2) {
+            return true;
+        }
+        if (x2 <= xCenter && xCenter <= x2 + radius && y1 <= yCenter &&
+            yCenter <= y2) {
+            return true;
+        }
+        if (distance(xCenter, yCenter, x1, y2) <= radius * radius) {
+            return true;
+        }
+        if (distance(xCenter, yCenter, x1, y1) <= radius * radius) {
+            return true;
+        }
+       
+        if (distance(xCenter, yCenter, x2, y2) <= radius * radius) {
+            return true;
+        }
+       
+        if (distance(xCenter, yCenter, x2, y1) <= radius * radius) {
+            return true;
+        }
+        return false;
+    }
+};
